dda535b23cff32bbc0babbade8d16a611b96079a
Seungmin7979 <safetmdals79@gmail.com>
2025-08-25 13:01:49 +0900

    [Feat] Step4 데이터와 UI 연동

--- Assets/02. Scripts/Manager/GameManager.cs
+++ Assets/02. Scripts/Manager/GameManager.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance;
+    public UserData userData;
+
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI cashText;
+    [SerializeField] private TextMeshProUGUI balanceText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        userData = new UserData("½Å½Â¹Î", 100000, 50000);
+    }
+
+    private void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        nameText.text = userData.UserName;
+        cashText.text = userData.cash.ToString("N0");
+        balanceText.text = userData.bankBalance.ToString("N0");
+    }
 }
